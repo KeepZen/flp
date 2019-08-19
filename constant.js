3fc6c7490497617d9ff5ad346b1d34cb8cf457ca
Zed <zed.zhang@newtype.io>
2019-08-19 12:36:29 +0800

    change meaning of `constant`

--- constant.js
+++ constant.js
@@ -1,25 +1,16 @@
 const deepCopy = require('./deep-copy');
 
 /**
- * Get a **immutable** copy of `value`.
- * @param {any} value the `value` which may be mutable.
- * @example
- * const assert = require('assert');
- * let obj = {a:1,b:2}
- * obj.add_a_and_b=function(){
- *  return this.c  = this.a + this.b;
- * }
- * let objc = constant(obj);
- * objc.a++;
- * assert.equal(obj.a,1);
- * objc.add_a_and_b();
- * assert.equal(objc.c,undefined);
- * @returns {any} An constant value.
+ * Give a value, get a function, which always return freezen deep copy of 
+ * the given value.
+ * 
+ * If you just want get a immutable copy, just use
+ * `deeopCopy(value,{freeze:true})`.
+ * @param {any} value 
+ * @return {Function} : ()=>deepCopyOfValue
  */
 const constant = (value) => {
-  if (value instanceof Object && value != null) {
-    value = deepCopy(value, { freeze: true });
-  }
-  return value;
+  const copy = deepCopy(value, { freeze: true });
+  return () => copy;
 }
 module.exports = constant;
--- constant.test.js
+++ constant.test.js
@@ -1,17 +1,17 @@
 const constant = require('./constant');
-test("imute(obj)", () => {
+test("constant(v)", () => {
   let obj = { a: 1, b: 2 }
   obj.add_a_and_b = function () {
     return this.c = this.a + this.b;
   }
-  let objc = constant(obj);
+  let objc = constant(obj)();
   objc.a++;
-  expect(obj.a).toBe(1);
+  expect(objc.a).toBe(1);
   delete objc.a;
-  expect(obj.a).toBe(1);
+  expect(objc.a).toBe(1);
   objc.add_a_and_b();
   expect(objc.c).toBe(undefined);
+  expect(objc.add_a_and_b()).toBe(3);
   objc.newProperty = 1;
   expect(objc.newProperty).toBe(undefined);
-  expect(objc.add_a_and_b()).toBe(3);
 });
